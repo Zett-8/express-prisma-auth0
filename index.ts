981b4870b77863dca1f5c5becfdb6c5903479b6a
Zett-8 <kame0008@gmail.com>
2021-11-12 21:19:45 +0100

    init express

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+const PORT = 3333
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(PORT, () => {
+  console.log('server is running on :3333')
+})
